500d188bc739f427108820a1bf25dc551af9a7ca
ilia moradi <iliamoradi.prog@gmail.com>
2024-04-12 22:06:49 +0330

    header title linked to main page

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,13 +1,16 @@
 import { AppBar, Container, Toolbar, Typography } from '@mui/material'
 import React from 'react'
 import BookIcon from '@mui/icons-material/Book';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <AppBar position='sticky'>
         <Container maxWidth="lg">
             <Toolbar>
-                <Typography component="h1" variant='h5' fontWeight="700" flex={1}>وبلاگ</Typography>
+                <Typography component="h1" variant='h5' fontWeight="700" flex={1}>
+                  <Link to="/" style={{textDecoration:"none", color:"white"}}>وبلاگ</Link>
+                </Typography>
                 <BookIcon />
             </Toolbar>
         </Container>
